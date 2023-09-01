601706f241745eb51b999a32f8d422aa938c86c1
Sabbir-Noyon <sabbir.noyon.001@gmail.com>
2023-09-01 23:52:10 +0600

    Update 2

--- script.js
+++ script.js
@@ -20,26 +20,73 @@ const openBlog = () => {
 
 const handleApiCategories = async () => {
 
-   
 
     const response = await fetch("https://openapi.programming-hero.com/api/videos/categories");
 
     const data = await response.json();
 
-    console.log(data.data);
+
+     // getting tab container for the dynamic tabs
+     const tabGetContainer = document.getElementById('tab-get-container');
+
+        data.data.forEach((apiCatagories)=> {
+
+        const div = document.createElement("div");
+
+        div.innerHTML=`
+
+        <a onclick = "allDataByClick('${apiCatagories.category_id}')" class="tab">${apiCatagories.category}</a> 
+        
+        
+        ` ;
 
 
+        tabGetContainer.appendChild(div);
 
-
-
-
-
+    } );
+    
+    
+     //console.log(data.data[0]);
 
 
 };
 
 
-handleApiCategories();
+
+
+
+
+
+
+    // on-clicking data 
+    const allDataByClick = (kolamula) => {
+
+        console.log(kolamula);
+
+
+   
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+    handleApiCategories();
+
+
 
 
 
